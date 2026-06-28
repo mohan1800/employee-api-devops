29a3ae627aa3e60041e365cbdc7e2998281d6348
mohan1800 <141727804+mohan1800@users.noreply.github.com>
2026-06-28 22:39:35 +0530

    Initial commit

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mohan",
+                    Department = "IT",
+                    Salary = 50000
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Rahul",
+                    Department = "HR",
+                    Salary = 40000
+                }
+            };
+
+            return Ok(employees);
+        }
+    }
+}
